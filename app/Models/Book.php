1ba7cfac067f52d8ada2c4dacde828f7870954f1
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-06-06 21:36:45 -0300

    🚧 Add: Book model

---
+++ app/Models/Book.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'author_id',
+        'quantity',
+        'available',
+    ];
+
+    /**
+     * Check if a book is available for borrowing.
+     *
+     * @param int $requestedQuantity The requested quantity to borrow.
+     * @return bool Returns true if the book is available, false otherwise.
+     */
+    public function isAvailableForBorrowing(int $requestedQuantity): bool
+    {
+        if ($this->quantity < $requestedQuantity || !$this->available) {
+            return false;
+        }
+        if(($this->quantity - $requestedQuantity) == 0) {
+            $this->available = false;
+        }
+
+        $this->decrement('quantity', $requestedQuantity);
+        $this->save();
+
+        return true;
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function borrow()
+    {
+        return $this->belongsTo(Borrowing::class);
+    }
+}
