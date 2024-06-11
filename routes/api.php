e01a4000743d1d86317e6eb379620c63133b8de2
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-11 00:12:23 -0300

    🚧add: Api route for categories

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\BorrowingController;
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,7 @@ Route::post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::apiResource('/books', BookController::class);
+    Route::apiResource('/categories', CategoryController::class);
     Route::apiResource('/authors', AuthorController::class);
     Route::apiResource('/borrow', BorrowingController::class);
 });
