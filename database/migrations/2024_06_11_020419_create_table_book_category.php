8490a253b73e007ceec91a23378350a3d7d679bc
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-11 00:23:48 -0300

    📦alter: table book_category down fix

--- database/migrations/2024_06_11_020419_create_table_book_category.php
+++ database/migrations/2024_06_11_020419_create_table_book_category.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('books_categories');
+        Schema::dropIfExists('book_category');
     }
 };
