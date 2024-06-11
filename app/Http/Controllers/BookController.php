c0f418f316e471a0ece18e200af8c1d96fe2d548
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-11 00:09:23 -0300

    🚧update: BookController add with index, updating validate, attach and sync to categories

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -12,7 +12,7 @@ class BookController extends Controller
      */
     public function index()
     {
-        $books = Book::with('author')->get();
+        $books = Book::with('author')->with('categories')->get();
         return response()->json(['success' => true, 'msg' => 'Listando livros', 'data' => $books]);
     }
 
@@ -21,20 +21,34 @@ class BookController extends Controller
      */
     public function store(Request $request)
     {
+        // TO DO - Adicionar campo que registra a quantidade de livros que foi emprestado
+        // TO DO - Adicionar uma opção de mandar o id do author pela rota também
         try {
-            $request->validate([
+            $request->validate(
+                [
                     'title' => 'required|string|max:255',
-                    'description' => 'required|string|nullable',
+                    'description' => 'nullable|string',
                     'author_id' => 'required|int',
-                ], [
+                    'quantity' => 'required|int',
+                    'available' => 'nullable|int',
+                    'categories' => 'nullable|array',
+                ],
+                [
                     'required' => 'O campo :attribute é obrigatório',
                     'max' => 'O campo :attribute tem que ser 255 caracteres',
                     'string' => 'O campo :attribute tem que ser do tipo string',
                     'int' => 'O campo :attribute tem que ser do tipo integer',
+                    'array' => 'O campo :attribute tem que ser do tipo array'
                 ]
             );
 
-            $newBook = Book::create($request->only(['title', 'description', 'author_id']));
+            $newBook = Book::create($request->only(['title', 'description', 'author_id', 'quantity', 'available']));
+
+            if ($request->categories) {
+                $newBook->categories()->attach($request->categories);
+            }
+
+            $newBook->save();
 
             return response()->json(['success' => true, 'msg' => 'Livro criado com sucesso!', 'data' => $newBook], 200);
         } catch (\Throwable $th) {
@@ -61,23 +75,34 @@ class BookController extends Controller
     public function update(Request $request, string $id)
     {
         try {
-            $request->validate([
+            $request->validate(
+                [
                     'title' => 'required|string|max:255',
-                    'description' => 'required|string|nullable',
+                    'description' => 'nullable|string',
                     'author_id' => 'required|int',
                     'quantity' => 'required|int',
-                    'available' => 'required|int',
-                ], [
+                    'available' => 'nullable|int',
+                    'categories' => 'nullable|array',
+                ],
+                [
                     'required' => 'O campo :attribute é obrigatório',
                     'max' => 'O campo :attribute tem que ser 255 caracteres',
                     'string' => 'O campo :attribute tem que ser do tipo string',
                     'int' => 'O campo :attribute tem que ser do tipo integer',
+                    'array' => 'O campo :attribute tem que ser do tipo array'
                 ]
             );
 
             $book = Book::findOrFail($id);
-            $book->update($request->only(['title', 'description', 'author_id']));
-            $book->save();
+            $book->update($request->only(['title', 'description', 'author_id', 'quantity', 'available']));
+
+            if ($request->categories) {
+                $book->categories()->sync($request->categories);
+            } else {
+                $book->categories()->deatch();
+            }
+
+            $book->refresh();
 
             return response()->json(['success' => true, 'msg' => 'Livro atualizado com sucesso!', 'data' => $book], 200);
         } catch (\Throwable $th) {
