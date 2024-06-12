b78e60be7b96dc6f314169abca335c9937d9053a
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-12 14:07:18 -0300

    🚧update: BorrowingController return msgs to portuguese

--- app/Http/Controllers/BorrowingController.php
+++ app/Http/Controllers/BorrowingController.php
@@ -5,19 +5,20 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\Borrowing;
 use Illuminate\Http\Request;
-use Ramsey\Uuid\Type\Integer;
 
 class BorrowingController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    
+    // TODO - Adicionar uma multa de atraso para retorno
     public function index()
     {
         $borrows = Borrowing::with('user', 'book')->get();
         return response()->json([
             'success' => true,
-            'message' => 'All borrows',
+            'message' => 'Listando todos os emprestimos',
             'data' => $borrows
         ]);
     }
@@ -30,15 +31,16 @@ class BorrowingController extends Controller
         // TO DO - Adicionar campo que registra a quantidade de livros que foi emprestado
         try {
             $request->validate([
-                'user_id' => 'required|int',
-                'book_id' => 'required|int',
+                'user_id' => 'required|int|exists: users, id',
+                'book_id' => 'required|int|exists: books, id',
                 'quantity' => 'required|int',
                 'borrow_date' => 'required|date',
                 'return_date' => 'required|date'
             ], [
-                'required' => 'The :attribute field is required.',
-                'int' => 'The :attribute must be an integer.',
-                'date' => 'The :attribute must be a date.',
+                'required' => 'O campo :attribute é obrigatório',
+                'int' => 'O campo :attribute deve ser um integer',
+                'date' => 'O campo :attribute deve ser uma data',
+                'exist' => 'O campo :attribute não existe'
             ]);
 
             $book = $request->input('book_id');
@@ -46,19 +48,18 @@ class BorrowingController extends Controller
             $quantity = $request->input('quantity');
             $bookAvailable = $book->isAvailableForBorrowing($quantity);
 
-            // dd($bookAvailable);
             if ($book->available = 0) {
-                return response()->json(['success' => false, 'msg' => 'Book not available', 'data' => null]);
+                return response()->json(['success' => false, 'msg' => 'Livro indisponível', 'data' => null]);
             }
 
             if ($bookAvailable == false) {
-                return response()->json(['success' => false, 'msg' => 'Quantity not available', 'data' => null]);
+                return response()->json(['success' => false, 'msg' => 'Quantidade indisponível', 'data' => null]);
             }
 
             $borrow = Borrowing::create($request->only(['user_id', 'book_id', 'borrow_date', 'return_date']));
-            return response()->json(['success' => true, 'msg' => 'Borrow created successfully', 'data' => $borrow]);
+            return response()->json(['success' => true, 'msg' => 'Emprestimo realizado', 'data' => $borrow]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Something went wrong', 'data' => $th->getMessage()]);
+            return response()->json(['success' => false, 'msg' => 'Não foi possível realizar o emprestimo', 'data' => $th->getMessage()]);
         }
     }
 
@@ -69,9 +70,9 @@ class BorrowingController extends Controller
     {
         try {
             $borrow = Borrowing::find($id);
-            return response()->json(['success' => true, 'msg' => 'Borrow retrieved successfully', 'data' => $borrow]);
+            return response()->json(['success' => true, 'msg' => 'Emprestimo encontrado', 'data' => $borrow]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Something went wrong', 'data' => $th->getMessage()]);
+            return response()->json(['success' => false, 'msg' => 'Não foi possível encontrar o emprestimo', 'data' => $th->getMessage()]);
         }
     }
 
@@ -82,15 +83,16 @@ class BorrowingController extends Controller
     {
         try {
             $request->validate([
-                'user_id' => 'required|int',
-                'book_id' => 'required|int',
+                'user_id' => 'required|int|exists: users, id',
+                'book_id' => 'required|int|exists: books, id',
                 'quantity' => 'required|int',
                 'borrow_date' => 'required|date',
                 'return_date' => 'required|date'
             ], [
-                'required' => 'The :attribute field is required.',
-                'int' => 'The :attribute must be an integer.',
-                'date' => 'The :attribute must be a date.',
+                'required' => 'O campo :attribute é obrigatório',
+                'int' => 'O campo :attribute deve ser um integer',
+                'date' => 'O campo :attribute deve ser uma data',
+                'exist' => 'O campo :attribute não existe'
             ]);
 
             $book = $request->input('book_id');
@@ -98,19 +100,19 @@ class BorrowingController extends Controller
             $quantity = $request->input('quantity');
 
             if ($book->quantity <= 0 || $book->available = 0) {
-                return response()->json(['success' => false, 'msg' => 'Book not available', 'data' => null]);
+                return response()->json(['success' => false, 'msg' => 'Livro indisponível', 'data' => null]);
             }
 
             if ($book->quantity < $quantity) {
-                return response()->json(['success' => false, 'msg' => 'Quantity not available', 'data' => null]);
+                return response()->json(['success' => false, 'msg' => 'Quantidade indisponível', 'data' => null]);
             }
 
             $borrowing = Borrowing::find($id)
                 ->update($request->only(['user_id', 'book_id', 'borrow_date', 'return_date']));
 
-            return response()->json(['success' => true, 'msg' => 'Borrow updated successfully', 'data' => $borrowing]);
+            return response()->json(['success' => true, 'msg' => 'emprestimo atualizado', 'data' => $borrowing]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Something went wrong', 'data' => $th->getMessage()]);
+            return response()->json(['success' => false, 'msg' => 'Não foi possível atualizar o emprestimo', 'data' => $th->getMessage()]);
         }
     }
 
@@ -121,9 +123,9 @@ class BorrowingController extends Controller
     {
         try {
             $borrowing = Borrowing::findOrFail($id)->delete();
-            return response()->json(['success' => true, 'msg' => 'Borrow deleted successfully']);
+            return response()->json(['success' => true, 'msg' => 'Emprestimo deletado com sucesso', 'data' => $borrowing]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Something went wrong', 'data' => $th->getMessage()]);
+            return response()->json(['success' => false, 'msg' => 'Não foi possível deletar o emprestimo', 'data' => $th->getMessage()]);
         }
     }
 }
