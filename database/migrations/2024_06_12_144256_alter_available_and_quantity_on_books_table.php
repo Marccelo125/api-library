777a6f1e571a0a7cfd517e2682271c7f3b218f29
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-12 14:54:34 -0300

    📦alter: table borrowings available and quantity order

---
+++ database/migrations/2024_06_12_144256_alter_available_and_quantity_on_books_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->integer('available')->after('author_id');
+            $table->integer('quantity')->after('available');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('available');
+            $table->dropColumn('quantity');
+        });
+    }
+};
