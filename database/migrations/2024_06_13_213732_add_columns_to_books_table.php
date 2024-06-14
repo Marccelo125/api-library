de64acbd26b5cbf97e04805456f897441f595fc1
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-14 15:51:50 -0300

    🚧add: columns on books table

---
+++ database/migrations/2024_06_13_213732_add_columns_to_books_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->integer('total_pages')->after('author_id');
+            $table->string('language')->after('total_pages');
+            $table->string('publisher')->after('language');
+            $table->date('publish_date')->nullable()->after('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('total_pages');
+            $table->dropColumn('publish_date');
+            $table->dropColumn('language');
+            $table->dropColumn('publisher');
+        });
+    }
+};
