57442292b31eb3710933db44bdc388e9ac60f825
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-14 15:45:57 -0300

    🚧update: AuthorController converting returns to ApiResponse and add ratings

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,49 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Models\Book;
+use App\Responses\ApiResponse;
+use App\Services\BookService;
 use Illuminate\Http\Request;
 
 // TODO - Adicionar uma opção de mandar o id do author pela rota também (store)
-// TODO - (ratings - many to many table)
-// Adicionar Avaliações para os livros (1 livro pode ter muitas avaliações e 1 avaliação pode ter muitos livros)
 
 class BookController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $books = Book::with('author')->with('categories')->get();
-        return response()->json(['success' => true, 'msg' => 'Listando livros', 'data' => $books]);
+        return ApiResponse::success('Listando livros!', $books);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         try {
-            $request->validate(
-                [
-                    'title' => 'required|string|max:255',
-                    'description' => 'nullable|string',
-                    'author_id' => 'required|int',
-                    'quantity' => 'required|int',
-                    'available' => 'nullable|int',
-                    'categories' => 'nullable|array',
-                ],
-                [
-                    'required' => 'O campo :attribute é obrigatório',
-                    'max' => 'O campo :attribute tem que ser 255 caracteres',
-                    'string' => 'O campo :attribute tem que ser do tipo string',
-                    'int' => 'O campo :attribute tem que ser do tipo integer',
-                    'array' => 'O campo :attribute tem que ser do tipo array',
-                    'exists' => 'O campo :attribute não existe'
-                ]
-            );
-
-            $newBook = Book::create($request->only(['title', 'description', 'author_id', 'quantity', 'available']));
+            $validatedRequest = BookService::validateRequest($request);
+            $newBook = Book::create($validatedRequest);
 
             if ($request->categories) {
                 $newBook->categories()->attach($request->categories);
@@ -53,52 +29,29 @@ class BookController extends Controller
 
             $newBook->save();
 
-            return response()->json(['success' => true, 'msg' => 'Livro criado com sucesso!', 'data' => $newBook], 200);
+            return ApiResponse::success('Livro criado com sucesso!', [$newBook]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Não foi possível criar o livro!', 'data' => $th->getMessage()], 400);
+            return ApiResponse::fail('Não foi possível criar o livro!', [$th->getMessage()]);
         }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
         try {
             $book = Book::with('author')->FindOrFail($id);
-            return response()->json(['success' => true, 'msg' => 'Livro encontrado com sucesso!', 'data' => $book], 200);
+            return ApiResponse::success('Livro encontrado com sucesso!', [$book]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Não foi possível encontrar o livro!', 'data' => $th->getMessage()], 400);
+            return ApiResponse::fail('Não foi possível encontrar o livro!', [$th->getMessage()]);
         }
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, string $id)
     {
         try {
-            $request->validate(
-                [
-                    'title' => 'required|string|max:255',
-                    'description' => 'nullable|string',
-                    'author_id' => 'required|int',
-                    'quantity' => 'required|int',
-                    'available' => 'nullable|int',
-                    'categories' => 'nullable|array'
-                    ],
-                    [
-                    'required' => 'O campo :attribute é obrigatório',
-                    'max' => 'O campo :attribute tem que ser 255 caracteres',
-                    'string' => 'O campo :attribute tem que ser do tipo string',
-                    'int' => 'O campo :attribute tem que ser do tipo integer',
-                    'array' => 'O campo :attribute tem que ser do tipo array',
-                    'exists' => 'O campo :attribute não existe'
-                ]
-            );
+            $validatedRequest = BookService::validateRequest($request);
 
             $book = Book::findOrFail($id);
-            $book->update($request->only(['title', 'description', 'author_id', 'quantity', 'available']));
+            $book->update($validatedRequest);
 
             if ($request->categories) {
                 $book->categories()->sync($request->categories);
@@ -108,24 +61,21 @@ class BookController extends Controller
 
             $book->refresh();
 
-            return response()->json(['success' => true, 'msg' => 'Livro atualizado com sucesso!', 'data' => $book], 200);
+            return ApiResponse::success('Livro atualizado com sucesso!', [$book]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Não foi possível atualizar este livro!', 'data' => $th->getMessage()], 400);
+            return ApiResponse::fail('Não foi possível atualizar este livro!', [$th->getMessage()]);
         }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
         try {
             $book = Book::FindOrFail($id);
             $book->delete();
 
-            return response()->json(['success' => true, 'msg' => 'Livro deletado com sucesso!', 'data' => $book], 200);
+            return apiResponse::success('Livro deletado com sucesso!', [$book]);
         } catch (\Throwable $th) {
-            return response()->json(['success' => false, 'msg' => 'Não foi possível deletar este livro!', 'data' => $th->getMessage()], 400);
+            return ApiResponse::fail('Não foi possível deletar o livro!', [$th->getMessage()]);
         }
     }
 }
