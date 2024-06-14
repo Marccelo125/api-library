dcdf87a44269fdf8d17ea76f91c7b9b49d4c3db1
Marcelo Duarte <contato.marccelo125@gmail.com>
2024-06-14 15:47:41 -0300

    ✅create: RatingController

---
+++ app/Http/Controllers/RatingController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Rating;
+use Illuminate\Http\Request;
+
+class RatingController extends Controller
+{
+    // TODO - CRUD rating
+    // TODO - Converter returns to apiResponse
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Rating $rating)
+    {
+        //
+    }
+
+    public function update(Request $request, Rating $rating)
+    {
+        //
+    }
+
+    public function destroy(Rating $rating)
+    {
+        //
+    }
+}
