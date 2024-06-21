d1f129a788727441e3ea96ac948e3675987049ce
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-06-21 01:51:52 -0300

    🚧 add: RatingController simples CRUD in working

--- app/Http/Controllers/RatingController.php
+++ app/Http/Controllers/RatingController.php
@@ -9,16 +9,21 @@ use Number;
 
 class RatingController extends Controller
 {
-    // TODO - Terminar CRUD rating
+    // TODO - adicionar uma verificação se aquele usuario fez uma borrowing neste livro
+    // só assim ele vai poder fazer uma avaliação
     public function index()
     {
-        //
+        try {
+            $ratings = Rating::all();
+            return ApiResponse::success('Listando avaliações', [$ratings]);
+        } catch (\Throwable $th) {
+            return ApiResponse::fail('Não foi possível retornar as avaliacoes.', [$th->getMessage()]);
+        }
     }
 
     public function store(Request $request)
     {
         try {
-
             $request->validate([
                 'rating' => 'required|string',
                 'comment' => 'required|string',
@@ -29,21 +34,22 @@ class RatingController extends Controller
                 'exists' => 'O dado no campo :attribute não existe',
                 'string' => 'O campo :attribute deve ser uma string',
             ]);
-    
+
             $alreadyRated = Rating::alreadyRated($request);
-    
+
             if ($alreadyRated) {
                 return ApiResponse::fail('Avaliação ja foi atribuida.', [null]);
             }
-    
+
             if ($request->rating > 5 || $request->rating < 0) {
                 return ApiResponse::fail('A avaliação deve ter valor inteiro entre 0 e 5.', [null]);
             }
-    
-            if (strpos((string)$request->rating, '.') !== false) {
-                $request->rating = str_replace('.', ',', (string)$request->rating);
+
+            $ratingValue = (float) str_replace(',', '.', $request->input('rating'));
+            if ($ratingValue > 5 || $ratingValue < 0) {
+                return ApiResponse::fail('A avaliação deve ter valor inteiro entre 0 e 5.', [null]);
             }
-    
+
             $rating = Rating::create($request->only(['rating', 'comment', 'user_id', 'book_id']));
             return ApiResponse::success('Avaliação criada com sucesso.', [$rating]);
         } catch (\Throwable $th) {
@@ -51,18 +57,54 @@ class RatingController extends Controller
         }
     }
 
-    public function show(Rating $rating)
+    public function show(int $id)
     {
-        //
+        try {
+            $rating = Rating::findOrFail($id);
+            return ApiResponse::success('Avaliação encontrada', [$rating]);
+        } catch (\Throwable $th) {
+            return ApiResponse::fail('Não foi possível retornar as avaliacoes.', [$th->getMessage()]);
+        }
     }
 
-    public function update(Request $request, Rating $rating)
+    public function update(Request $request, int $id)
     {
-        //
+        try {
+            $request->validate([
+                'rating' => 'required|string',
+                'comment' => 'required|string',
+                'user_id' => 'required|exists:users,id',
+                'book_id' => 'required|exists:books,id',
+            ], [
+                'required' => 'O campo :attribute é obrigatório',
+                'exists' => 'O dado no campo :attribute não existe',
+                'string' => 'O campo :attribute deve ser uma string',
+            ]);
+
+            $alreadyRated = Rating::alreadyRated($request);
+
+            if ($alreadyRated) return ApiResponse::fail('Avaliação ja foi atribuida.', [null]);
+
+            if ($request->rating > 5 || $request->rating < 0) return ApiResponse::fail('A avaliação deve ter valor inteiro entre 0 e 5.', [null]);
+
+            $ratingValue = (float) str_replace(',', '.', $request->input('rating'));
+            if ($ratingValue > 5 || $ratingValue < 0) return ApiResponse::fail('A avaliação deve ter valor inteiro entre 0 e 5.', [null]);
+
+            $rating = Rating::findOrFail($id);
+            $rating->update($request->only(['rating', 'comment', 'user_id', 'book_id']));
+            return ApiResponse::success('Avaliação atualizada com sucesso.', [$rating]);
+        } catch (\Throwable $th) {
+            return ApiResponse::fail('Não foi possível atualizar a avaliação.', [$th->getMessage()]);
+        }
     }
 
     public function destroy(Rating $rating)
     {
-        //
+        try {
+            $rating->delete();
+            return ApiResponse::success('Avaliação excluída com sucesso.', [null]);
+        } catch (\Throwable $th) {
+            return ApiResponse::fail('Não foi possível excluir a avaliação.', [$th->getMessage()]);
+        }
     }
 }
